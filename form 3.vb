80cb3e6751dd479b5185cc5571dcab626c5e1a62
Joy-Teezer <joyteezer@gmail.com>
2025-02-21 18:34:33 +0300

    Add files via upload

---
+++ form 3.vb
@@ -0,0 +1,62 @@
+﻿
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+    Private Sub Button2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Oject Demonstration")
+    End Sub
+    Private Sub Button3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Display Test")
+    End Sub
+    Private Sub Button4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Font")
+    End Sub
+    Private Sub Button5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Sample Test")
+    End Sub
+    Private Sub Button6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Garamond")
+    End Sub
+    Private Sub Button7_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Magneto")
+    End Sub
+    Private Sub Button8_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Tahoma")
+    End Sub
+    Private Sub Button9_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Font Style")
+    End Sub
+    Private Sub Button10_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Italic")
+    End Sub
+    Private Sub Button11_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Bold")
+    End Sub
+    Private Sub Button12_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Bold Italic")
+    End Sub
+
+    Private Sub Button13_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Font Color")
+    End Sub
+
+    Private Sub Button14_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Green")
+    End Sub
+
+    Private Sub Form15_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Blue")
+    End Sub
+    Private Sub Button16_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Red")
+    End Sub
+    Private Sub Button17_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("load Picture")
+
+    End Sub
+    Private Sub Button18_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox(" Application Exit")
+    End Sub
+
+End Class
