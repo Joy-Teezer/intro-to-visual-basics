4acacd48d6949f2bc48b545b5c01aa7c27fce429
Joy-Teezer <joyteezer@gmail.com>
2025-02-21 18:48:41 +0300

    Add files via upload

---
+++ form4.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Welcomming Form")
+    End Sub
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Load object Demo")
+    End Sub
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Application exit")
+    End Sub
+
+End Class
